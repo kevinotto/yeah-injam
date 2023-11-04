9f4a3046550b858b0b0b933a1f386b681b47da0b
Andre364 <andre.alas400@gmail.com>
2023-11-04 10:42:00 +0200

    Made the prompt system actually functional

--- InJam Yeah game/Assets/Scripts/DialogueChoices.cs
+++ InJam Yeah game/Assets/Scripts/DialogueChoices.cs
@@ -17,16 +17,24 @@ public class DialogueChoices : MonoBehaviour
 
     public TextMeshProUGUI choiceOneText;
     public TextMeshProUGUI choiceTwoText;
+    public GameObject choiceOne;
+    public GameObject choiceTwo;
 
     private void Start()
     {
         //Enemy initiates talking
         kendrickPrompts = GameObject.Find("Player").GetComponent<PromptList>().kendrickPrompts;
         pIndex = 0;
+        StartCoroutine("EnemySpeech");
+        choiceOne.SetActive(false); 
+        choiceTwo.SetActive(false);
     }
 
     void GiveChoices() //Give the player choices
     {
+
+        choiceOne.SetActive(true);
+        choiceTwo.SetActive(true);
         choiceOneText.SetText(kendrickPrompts[pIndex].answerOne);
         choiceTwoText.SetText(kendrickPrompts[pIndex].answerTwo);
     }
@@ -37,19 +45,30 @@ public class DialogueChoices : MonoBehaviour
         switch (i)
         {
             case 0:
+                playerText.SetText(kendrickPrompts[pIndex].answerOne);
                 pIndex += 1; //Good answer
                 break;
             case 1:
+                playerText.SetText(kendrickPrompts[pIndex].answerTwo);
                 pIndex += 2; //Bad answer. Bad answers are always even numbers, good ones always odd
                 break;
         }
-        EnemySpeech();
+
+        choiceOne.SetActive(false);
+        choiceTwo.SetActive(false);
+
+        StartCoroutine("EnemySpeech");
     }
 
-    void EnemySpeech() //Called after player responds
+    IEnumerator EnemySpeech() //Called after player responds
     {
+        if (pIndex > 0)
+        {
+            yield return new WaitForSeconds(2);
+        }
         enemyText.SetText(kendrickPrompts[pIndex].question);
 
+        yield return new WaitForSeconds(1.5f);
         //Give player choices
         GiveChoices();
     }
--- InJam Yeah game/Assets/Scripts/PromptList.cs
+++ InJam Yeah game/Assets/Scripts/PromptList.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PromptList : MonoBehaviour
 {
-    public List<Prompt> kendrickPrompts = new List<Prompt>(500);
+    public List<Prompt> kendrickPrompts = new List<Prompt>();
     private void Start()
     {
         kendrickPrompts.Add(new Prompt("Hey there, Loner! It's been too long -- how have you been??", "Kendrick! I've been good, just caught up in life. How about you?", "Kendrick, it has been a while, but I'm quite busy these days.", 1));
